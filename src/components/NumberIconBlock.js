1cd43a1c1afe443e3c64c6a54f3a771cb24a8e4f
Revathi <revathi.palanisamy@spagylo.com>
2023-07-14 16:10:47 +0530

    numberblock

--- src/components/NumberIconBlock.js
+++ src/components/NumberIconBlock.js
@@ -8,19 +8,18 @@ export default function NumberIconBlock({ review }) {
 <section className="numbericonblock">
         <div className="container">
           <h3>How it Works for Supporters</h3>              
-         <Row>
-              <Col xl="3" lg="3" md="4" xs="6">
-                    <div><img src="/img/step#1.svg" alt ="one"></img>
-                        <p>Supporters select a card design, write a personal message and choose a donation amount</p>
-                    </div>
-                 </Col>
-              <Col xl="3" lg="3" md="4" xs="6">               
-                          <div><img src="/img/#2.png"alt="two"></img></div>      
-                        <p>They upload the names and addresses of the people they want to receive cards</p>         
-                      </Col>
+         <Row className="space">
+              <Col xl="3" lg="3" md="4" xs="6" >
+                <img src="/img/step_1.svg" alt ="one"></img>
+                 <p>Supporters select a card design, write a personal message and choose a donation amount</p>
+              </Col>
+              <Col xl="3" lg="3" md="4" xs="6" className="space">               
+                <img src="/img/step2.svg"alt="two"></img>      
+                  <p>They upload the names and addresses of the people they want to receive cards</p>         
+               </Col>
               <Col xl="3" lg="3" md="4" xs="6">           
-                         <div><img src = "/img/#3.png" alt="three"></img></div>
-                        <p>Cards are sent automatically. No addressing, stamping, or trips to the post office necessary.</p>
+                <img src = "/img/step_3.svg" alt="three"></img>
+                <p>Cards are sent automatically. No addressing, stamping, or trips to the post office necessary.</p>
               </Col>
           </Row>
         </div>
