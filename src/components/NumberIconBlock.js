4bb56d0656bc62bb65a0b54153c2bd24aea367c1
Nanthini <nanthini@spagylo.com>
2023-07-14 16:19:08 +0530

    product listing block updated

--- src/components/NumberIconBlock.js
+++ src/components/NumberIconBlock.js
@@ -6,22 +6,22 @@ export default function NumberIconBlock({ review }) {
   return (
 
 <section className="numbericonblock">
-        <div className="container">
-          <h3>How it Works for Supporters</h3>              
-         <Row className="space">
-              <Col xl="3" lg="3" md="4" xs="6" >
+ <div className="container">
+     <h3>How it Works for Supporters</h3>       
+    <Row className="space">
+       <Col xl="3" lg="3" md="4" xs="6" >
                 <img src="/img/step_1.svg" alt ="one"></img>
-                 <p>Supporters select a card design, write a personal message and choose a donation amount</p>
+        <p>Supporters select a card design, write a personal message and choose a donation amount</p>
               </Col>
-              <Col xl="3" lg="3" md="4" xs="6" className="space">               
-                <img src="/img/step2.svg"alt="two"></img>      
-                  <p>They upload the names and addresses of the people they want to receive cards</p>         
-               </Col>
-              <Col xl="3" lg="3" md="4" xs="6">           
+       <Col xl="3" lg="3" md="4" xs="6" className="space">       
+                <img src="/img/step2.svg"alt="two"></img>   
+         <p>They upload the names and addresses of the people they want to receive cards</p>     
+       </Col>
+       <Col xl="3" lg="3" md="4" xs="6">     
                 <img src = "/img/step_3.svg" alt="three"></img>
-                <p>Cards are sent automatically. No addressing, stamping, or trips to the post office necessary.</p>
-              </Col>
-          </Row>
-        </div>
-      </section>
+        <p>Cards are sent automatically. No addressing, stamping, or trips to the post office necessary.</p>
+       </Col>
+     </Row>
+    </div>
+   </section>
   )}
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Swiper from "../components/Swiper"
 
 import data from "../data/index.json"
 import Link from "next/link"
+import React,{useState} from "react"
 
 
 export async function getStaticProps() {
@@ -23,6 +24,7 @@ export async function getStaticProps() {
 }
 
 export default function Home() {
+
   return (
     <>
       <Swiper
@@ -43,194 +45,166 @@ export default function Home() {
         sectionClass="mb-2 home-full-slider-wrapper"
         eager
       />
-      {/*<section>
-        <Container fluid className="g-2">
-          <Row className="g-2">
-            {data.lookbook.map((card, index) => {
-              const columns = index < 2 ? { md: 6 } : { lg: 4 }
-              const type = index < 2 ? "big" : "small"
-              return (
-                <Col {...columns} key={index}>
-                  <CardLookbook data={card} cardType={type} />
-                </Col>
-              )
-            })}
-          </Row>
-        </Container>
-          </section> */}
-
       <section className="productListBlock py-6">
         <Container>
+          <h3 className="productListTitle">Start by Choosing Your Card Design</h3>
           <Row>
-            <Col xl="8" className="mx-auto text-center mb-5">
-              <h3 className="text-uppercase">Start by Choosing Your Card Design</h3>
-            </Col>
+            <div className="cardItem">
+              <Link href="#">
+                <a>
+                  <img className="img-fluid w-100" src="/img/Card2.jpg" alt="Card1" />
+                </a>
+              </Link>
+              <div className="productDetails">
+                <Row>
+                  <Col xs="7" lg="7">
+                    <h3 className="h6 text-uppercase">
+                      <Link href="#">
+                        <a className="productTitle">Calm</a>
+                      </Link>
+                    </h3>
+                    <span className="productPrice">$15.00 USD</span>
+                  </Col>
+                  <Col xs="5" lg="5" className="text-center align-self-center cartBtn">
+                    <Link href="#">
+                      <a>BUY A CARD</a>
+                    </Link>
+                  </Col>
+                </Row>
+              </div>
+            </div>
+            <div className="cardItem">
+              <Link href="#">
+                <a>
+                  <img className="img-fluid w-100" src="/img/Card2.jpg" alt="Card2" />
+                </a>
+              </Link>
+              <div className="productDetails">
+                <Row>
+                  <Col xs="7" lg="7">
+                    <h3 className="h6 text-uppercase">
+                      <Link href="#">
+                        <a className="productTitle">Calm</a>
+                      </Link>
+                    </h3>
+                    <span className="productPrice">$15.00 USD</span>
+                  </Col>
+                  <Col xs="5" lg="5" className="text-center align-self-center cartBtn">
+                    <Link href="#">
+                      <a>BUY A CARD</a>
+                    </Link>
+                  </Col>
+                </Row>
+              </div>
+            </div>
+            <div className="cardItem">
+              <Link href="#">
+                <a>
+                  <img className="img-fluid w-100" src="/img/productCart3.jpg" alt="Card3" />
+                </a>
+              </Link>
+              <div className="productDetails">
+                <Row>
+                  <Col xs="7" lg="7">
+                    <h3 className="h6 text-uppercase">
+                      <Link href="#">
+                        <a className="productTitle">Calm</a>
+                      </Link>
+                    </h3>
+                    <span className="productPrice">$15.00 USD</span>
+                  </Col>
+                  <Col xs="5" lg="5" className="text-center align-self-center cartBtn">
+                    <Link href="#">
+                      <a>BUY A CARD</a>
+                    </Link>
+                  </Col>
+                </Row>
+              </div>
+            </div>
+            <div className="cardItem">
+              <Link href="#">
+                <a>
+                  <img className="img-fluid w-100" src="/img/Card4.jpg" alt="Card4" />
+                </a>
+              </Link>
+              <div className="productDetails">
+                <Row>
+                  <Col xs="7" lg="7">
+                    <h3 className="h6 text-uppercase">
+                      <Link href="#">
+                        <a className="productTitle">Calm</a>
+                      </Link>
+                    </h3>
+                    <span className="productPrice">$15.00 USD</span>
+                  </Col>
+                  <Col xs="5" lg="5" className="text-center align-self-center cartBtn">
+                    <Link href="#">
+                      <a>BUY A CARD</a>
+                    </Link>
+                  </Col>
+                </Row>
+              </div>
+            </div>
+            <div className="cardItem">
+              <Link href="#">
+                <a>
+                  <img className="img-fluid w-100" src="/img/Card5.jpg" alt="Card5" />
+                </a>
+              </Link>
+              <div className="productDetails">
+                <Row>
+                  <Col xs="7" lg="7">
+                    <h3 className="h6 text-uppercase">
+                      <Link href="#">
+                        <a className="productTitle">Calm</a>
+                      </Link>
+                    </h3>
+                    <span className="productPrice">$15.00 USD</span>
+                  </Col>
+                  <Col xs="5" lg="5" className="text-center align-self-center cartBtn">
+                    <Link href="#">
+                      <a>BUY A CARD</a>
+                    </Link>
+                  </Col>
+                </Row>
+              </div>
+            </div>
+            <div className="cardItem">
+              <Link href="#">
+                <a>
+                  <img className="img-fluid w-100" src="/img/Card6.jpg" alt="Card6" />
+                </a>
+              </Link>
+              <div className="productDetails">
+                <Row>
+                  <Col xs="7" lg="7">
+                    <h3 className="h6 text-uppercase">
+                      <Link href="#">
+                        <a className="productTitle">Calm</a>
+                      </Link>
+                    </h3>
+                    <span className="productPrice">$15.00 USD</span>
+                  </Col>
+                  <Col xs="5" lg="5" className="text-center align-self-center cartBtn">
+                    <Link href="#">
+                      <a>BUY A CARD</a>
+                    </Link>
+                  </Col>
+                </Row>
+              </div>
+            </div>
           </Row>
-
-          <Row>
-            <Col xs="6" lg="6">
-              <div className="mb-4">
-                <Link href="#">
-                  <a>
-                    <img className="img-fluid w-100" src="/img/Card2.jpg" alt="Card1" />
-                  </a>
-                </Link>
-                <div className="py-2">
-                  <Row>
-                    <Col xs="6" lg="6">
-                      <h3 className="h6 text-uppercase mb-1">
-                        <Link href="#">
-                          <a className="text-dark">Calm</a>
-                        </Link>
-                      </h3>
-                      <span className="text-muted">$15.00 USD</span>
-                    </Col>
-                    <Col xs="6" lg="6" className="text-end align-self-center">
-                      <Link href="#">
-                        <a className="btn btn-sm">BUY A CARD</a>
-                      </Link>
-                    </Col>
-                  </Row>
-                </div>
-              </div>
-            </Col>
-            <Col xs="6" lg="6">
-              <div className="mb-4">
-                <Link href="#">
-                  <a>
-                    <img className="img-fluid w-100" src="/img/Card2.jpg" alt="Card2" />
-                  </a>
-                </Link>
-                <div className="py-2">
-                  <Row>
-                    <Col xs="6" lg="6">
-                      <h3 className="h6 text-uppercase mb-1">
-                        <Link href="#">
-                          <a className="text-dark">Calm</a>
-                        </Link>
-                      </h3>
-                      <span className="text-muted">$15.00 USD</span>
-                    </Col>
-                    <Col xs="6" lg="6" className="text-end align-self-center">
-                      <Link href="#">
-                        <a className="btn btn-sm">BUY A CARD</a>
-                      </Link>
-                    </Col>
-                  </Row>
-                </div>
-              </div>
-            </Col>
-            <Col xs="6" lg="6">
-              <div className="mb-4">
-                <Link href="#">
-                  <a>
-                    <img className="img-fluid w-100" src="/img/Card3.jpg" alt="Card3" />
-                  </a>
-                </Link>
-                <div className="py-2">
-                  <Row>
-                    <Col xs="6" lg="6">
-                      <h3 className="h6 text-uppercase mb-1">
-                        <Link href="#">
-                          <a className="text-dark">Calm</a>
-                        </Link>
-                      </h3>
-                      <span className="text-muted">$15.00 USD</span>
-                    </Col>
-                    <Col xs="6" lg="6" className="text-end align-self-center">
-                      <Link href="#">
-                        <a className="btn btn-sm">BUY A CARD</a>
-                      </Link>
-                    </Col>
-                  </Row>
-                </div>
-              </div>
-            </Col>
-            <Col xs="6" lg="6">
-              <div className="mb-4">
-                <Link href="#">
-                  <a>
-                    <img className="img-fluid w-100" src="/img/Card4.jpg" alt="Card4" />
-                  </a>
-                </Link>
-                <div className="py-2">
-                  <Row>
-                    <Col xs="6" lg="6">
-                      <h3 className="h6 text-uppercase mb-1">
-                        <Link href="#">
-                          <a className="text-dark">Calm</a>
-                        </Link>
-                      </h3>
-                      <span className="text-muted">$15.00 USD</span>
-                    </Col>
-                    <Col xs="6" lg="6" className="text-end align-self-center">
-                      <Link href="#">
-                        <a className="btn btn-sm">BUY A CARD</a>
-                      </Link>
-                    </Col>
-                  </Row>
-                </div>
-              </div>
-            </Col>
-            <Col xs="6" lg="6">
-              <div className="mb-4">
-                <Link href="#">
-                  <a>
-                    <img className="img-fluid w-100" src="/img/Card5.jpg" alt="Card5" />
-                  </a>
-                </Link>
-                <div className="py-2">
-                  <Row>
-                    <Col xs="6" lg="6">
-                      <h3 className="h6 text-uppercase mb-1">
-                        <Link href="#">
-                          <a className="text-dark">Calm</a>
-                        </Link>
-                      </h3>
-                      <span className="text-muted">$15.00 USD</span>
-                    </Col>
-                    <Col xs="6" lg="6" className="text-end align-self-center">
-                      <Link href="#">
-                        <a className="btn btn-sm">BUY A CARD</a>
-                      </Link>
-                    </Col>
-                  </Row>
-                </div>
-              </div>
-            </Col>
-            <Col xs="6" lg="6">
-              <div className="mb-4">
-                <Link href="#">
-                  <a>
-                    <img className="img-fluid w-100" src="/img/Card6.jpg" alt="Card6" />
-                  </a>
-                </Link>
-                <div className="py-2">
-                  <Row>
-                    <Col xs="6" lg="6">
-                      <h3 className="h6 text-uppercase mb-1">
-                        <Link href="#">
-                          <a className="text-dark">Calm</a>
-                        </Link>
-                      </h3>
-                      <span className="text-muted">$15.00 USD</span>
-                    </Col>
-                    <Col xs="6" lg="6" className="text-end align-self-center">
-                      <Link href="#">
-                        <a className="btn btn-sm">BUY A CARD</a>
-                      </Link>
-                    </Col>
-                  </Row>
-                </div>
-              </div>
-            </Col>
-          </Row>
-          <p className="text-center">
-            LOAD MORE <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <g id="Icon/Small/Arrow Right">
-            <path id="Vector" d="M2.27161e-06 8.83332L12.5955 8.83332L9.40888 11.8208L10.6667 13L16 8L10.6667 3L9.40888 4.17918L12.5955 7.16668L2.41731e-06 7.16668L2.27161e-06 8.83332Z" fill="#FA6044"/>
-            </g>
-            </svg>
+          <p className="text-center loadMoreLink">
+            <Link href="#">
+              <a>
+                LOAD MORE 
+                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <g id="Icon/Small/Arrow Right">
+                  <path id="Vector" d="M2.27161e-06 8.83332L12.5955 8.83332L9.40888 11.8208L10.6667 13L16 8L10.6667 3L9.40888 4.17918L12.5955 7.16668L2.41731e-06 7.16668L2.27161e-06 8.83332Z" fill="#FA6044"/>
+                  </g>
+                </svg>
+              </a>
+            </Link>
           </p>
         </Container>
       </section>
