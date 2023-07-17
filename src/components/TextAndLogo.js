22e519e38c6d8d551ae8a83f63f3058831c2a36f
Revathi <revathi.palanisamy@spagylo.com>
2023-07-17 18:21:09 +0530

    textand logo

--- src/components/TextAndLogo.js
+++ src/components/TextAndLogo.js
@@ -7,24 +7,35 @@ export default function TextAndLogo({ review }) {
       <Container>
         <Row className="textandlogo">
           <Col lg="6" md="6" xs="12">
+            
             <div className="textandlogo_content">
               <h2>God's Love We Deliver</h2>
               <p>
-                God’s Love We Deliver cooks and home-delivers nutritious, medically tailored meals for people too sick to shop or
-                cook for themselves. To support the health of our clients, we also provide ongoing nutrition assessment,
-                education, and counseling. All of our services are provided free to clients and full of love.
+                God’s Love We Deliver cooks and home-delivers nutritious,
+                medically tailored meals for people too sick to shop or cook for
+                themselves. To support the health of our clients, we also
+                provide ongoing nutrition assessment, education, and counseling.
+                All of our services are provided free to clients and full of
+                love.
               </p>
-              <a className="primary_btn" href="#">
-                our website
-              </a>
+              <div className="centerbutton" >
+                <a className="link" href="#">our website</a>
+                <img src="/img/Arrow Right.png"></img>
+              </div>
             </div>
           </Col>
           <Col lg="6" md="6" xs="12">
-            <div>
-              <img className="w-100" src="/img/Logo_God's Love.png" />
+          <img src="/img/paper_top.png" class="paper_top" alt="paper-top" />
+          {/* <img src = "/img/paper_left.svg"></img> */}
+          {/* <img src = "/img/paper_right.svg"></img> */}
+          <div className="textandlogo_bgimage">
+            <div className="logoimage">
+              <img src="/img/God's Love_logo.png" />
               <h3>Donate to God's Love We Deliver</h3>
               <a href="#">donate now</a>
             </div>
+            </div>
+            <img src="/img/paper_bottom.png" class="paper_bottom" alt="paper-bottom"></img>
           </Col>
         </Row>
       </Container>
